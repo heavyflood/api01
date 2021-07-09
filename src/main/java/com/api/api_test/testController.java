cd723e951bcab6c2e4f34dc898238b54a6f2ae05
Hong-Su Park <heavyflood@gmail.com>
2021-07-09 17:31:59 +0900

    k8s deploy 수정

--- src/main/java/com/api/api_test/testController.java
+++ src/main/java/com/api/api_test/testController.java
@@ -3,11 +3,22 @@ package com.api.api_test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class testController {
 
     @GetMapping("/")
     public String getString() {
-        return "hi~roo~!!";
+
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            String strIP = addr.getHostAddress();
+            String strHostName = addr.getHostName();
+            return "return from "+ strHostName + "(" + strIP + ")";
+        }catch(UnknownHostException e){
+            return "Unknnnnown Host";
+        }
     }
 }
